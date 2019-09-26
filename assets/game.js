7b300029fa1ff2303b6ba51b8cefbfccfa58a8d9
Penelope A <penelope.arnold@du.edu>
2019-09-26 15:58:33 -0600

    Finishing touches
    updated html
    reworked JS to print guessed letters and properly update attempts

--- assets/game.js
+++ assets/game.js
@@ -1,55 +1,68 @@
 //VARIABLES
 var letters = ["a","b","c","d","e","f"];
-var randomLetter = letters[Math.floor(Math.random()*letters.length)];
-var win = 0
-var losses = 0
-var attempts = 10
+
 var guessedLetters = [];
 
-//generates random letter from the "letters" array using math.floor and math.random
-function guess () {
-    randomLetter = letters[Math.floor(Math.random()*letters.length)];
+var randomLetter = null
+
+var attempts = 10
+
+var win = 0
+
+var losses = 0
+
+var updateAttempts = function(){
+    document.querySelector("#remainingguess").innerHTML = attempts;
+}
+
+var chooseRandom = function(){
+    randomLetter = letters[Math.floor(Math.random() * letters.length)];
     console.log(randomLetter);
+};
+
+var updateGuessed = function(){
+    document.querySelector("#guessed").innerHTML = guessedLetters.join(", ")
+    console.log(guessedLetters);
+};
+
+updateAttempts();
+
+var reset = function(){
+    attempts = 10;
+    guessedLetters = [];
+    chooseRandom();
+    updateAttempts();
+    updateGuessed();
+}
+
+chooseRandom();
+updateGuessed();
+
+document.onkeydown = function(event){
+    attempts--;
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    guessedLetters.push(letter);
+    updateAttempts();
+    updateGuessed();
+
+    if(letter === randomLetter){
+        win++;
+        document.querySelector("#wins").innerHTML = win
+        
+        reset();
+    }
+
+
+    if(attempts === 0){
+        losses++;
+        document.querySelector("#losses").innerHTML = losses
+
+        reset();
+    }
+
 }
 
 
-// capturing user input: 
-  
-    //incorrect guesses: if 
-  guess();  
-  document.onkeyup = function (event) {
-    var playerGuess = event.key;
-    console.log(playerGuess);
-    if (playerGuess !== randomLetter){
-        attempts--; 
-        console.log(attempts);
-        //document.querySelector("#remainingguess").innerHTML = attempts
-        //document.getElementById("remainingguess").innerHTML = attempts
-        guessedLetters.push(playerGuess);
-        console.log(guessedLetters);
-        console.log("not equal");
-        if (attempts == 0){
-            losses++;
-        }
-    } else if(playerGuess === randomLetter) {
-        console.log("equal");
-        win++
-        console.log(win);
-    //call reset function    
-    }
 
-    //reset function: empty guessed letters array, reset attempts to 10, new random letter, clear/reset html
 
-    //running out of guesses, empties guessedLetters and resets attempts
-    // // if (attempts == 0);{
-    //     losses ++;
-    //     guessedLetters = [];
-    //     attempts = 10
-   // }
-    //output of incorrect guesses 
-    // if (guessedLetters.indexOf(playerGuess) >=0);{
-    //      document.getElementById("alreadyguessed").innerHTML = guessedLetters;
-    //      console.log(guessedLetters);
-        }
-    // }
-//Display: push to inner HTML 
+
